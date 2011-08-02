fcc7f0ac1b296890606def0fcc375aba52dc8c1e
Jason Thomas <jason@lithiumfox.com>
2011-08-02 00:04:49 -0400

    added function last + memcache key storage

--- teddy.pl
+++ teddy.pl
@@ -6,6 +6,7 @@ import ircbot
 import sys
 import re
 import mechanize
+import memcache
 
 network = '127.0.0.1'
 port = 6667
@@ -20,7 +21,15 @@ class TeddyBot (ircbot.SingleServerIRCBot):
     def on_welcome (self, connection, event):
         connection.join ( channel )
 
-    def on_pubmsg ( self, connection, event ):
+    def memcache_write(self, key, value):
+        mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+        mc.set(key,value)
+
+    def memcache_read(self, key):
+        mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+        return mc.get(key)
+
+    def on_pubmsg (self, connection, event):
         source = event.source().split ('!') [0]
         channel = event.target()
         msg = event.arguments()[0]
@@ -29,19 +38,27 @@ class TeddyBot (ircbot.SingleServerIRCBot):
             connection.privmsg(channel, 'hello ' + source)
         if msg.lower().startswith("!%s" % "penis"):
             connection.privmsg(channel, source + ' loves the pee pee')
+        if msg.lower().startswith("!%s" % "last"):
+            parse_last = re.split(' ', msg.strip())
+            if len(parse_last) == 2 :
+               connection.privmsg(channel, self.memcache_read(parse_last[1]))
+            else :
+                connection.privmsg(channel, "!last username")
         if msg.lower().startswith("!%s" % "dance"):
             connection.privmsg(channel, ":D\<")
             connection.privmsg(channel, ":D|<")
             connection.privmsg(channel, ":D/<")
             connection.privmsg(channel, ":D|<")
-        if msg.lower().startswith("http:"):
+        if msg.lower().startswith("http"):
             try:
                 browser = mechanize.Browser()
                 browser.open(msg)
+                self.memcache_write(source,msg)
                 connection.privmsg(channel, browser.title())
             except IOError:
                 connection.privmsg(channel, "jason didnt design me with proper logic")
             except:
                 connection.privmsg(channel, source + " what did you send me?")
+         
 bot = TeddyBot ([( network, port )], nick, name)
 bot.start() 
