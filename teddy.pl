56685968ecddb5995d63d8d42ab57a44b0bf21cd
Jason Thomas <jason@lithiumfox.com>
2011-08-01 21:59:51 -0400

    fix bug with empty error response, thanks dumitru for breaking the bot

--- teddy.pl
+++ teddy.pl
@@ -41,5 +41,7 @@ class TeddyBot (ircbot.SingleServerIRCBot):
                 connection.privmsg(channel, browser.title())
             except IOError:
                 connection.privmsg(channel, "jason didnt design me with proper logic")
+            except:
+                connection.privmsg(channel, source + " what did you send me?")
 bot = TeddyBot ([( network, port )], nick, name)
 bot.start() 
