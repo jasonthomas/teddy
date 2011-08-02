2ffc1fa36d0b776eab3fa6caff0378b813d0cf6f
Jason Thomas <jason@lithiumfox.com>
2011-08-02 11:24:47 -0400

    using redis instead memcached to make use of hash storage

--- teddy.pl
+++ teddy.pl
@@ -6,7 +6,10 @@ import ircbot
 import sys
 import re
 import mechanize
-import memcache
+import redis
+import time
+import random
+import string
 
 network = '127.0.0.1'
 port = 6667
@@ -14,20 +17,28 @@ channel = '#chat'
 nick = 'teddy'
 name = 'teddy'
 
-statistics = {}
-
+redis_server = redis.Redis("192.168.0.70")
 
 class TeddyBot (ircbot.SingleServerIRCBot):
     def on_welcome (self, connection, event):
         connection.join ( channel )
 
-    def memcache_write(self, key, value):
-        mc = memcache.Client(['127.0.0.1:11211'], debug=0)
-        mc.set(key,value)
+    def gen_random_string(self):
+        char_set = string.ascii_lowercase + string.digits
+        return ''.join(random.sample(char_set,10))
 
-    def memcache_read(self, key):
-        mc = memcache.Client(['127.0.0.1:11211'], debug=0)
-        return mc.get(key)
+    def redis_write(self, source, url, title):
+        short = self.gen_random_string()
+        redis_server.hset(url, "short", short)
+        redis_server.hset(url, "title", title)
+        redis_server.hset(url, "source", source)
+        redis_server.hset(url, "time", time.gmtime())
+
+    def redis_write_last(self, key, value):
+        redis_server.set(key,value)
+
+    def redis_read_last(self, key):
+        return redis_server.get(key)
 
     def on_pubmsg (self, connection, event):
         source = event.source().split ('!') [0]
@@ -41,7 +52,7 @@ class TeddyBot (ircbot.SingleServerIRCBot):
         if msg.lower().startswith("!%s" % "last"):
             parse_last = re.split(' ', msg.strip())
             if len(parse_last) == 2 :
-               connection.privmsg(channel, self.memcache_read(parse_last[1]))
+               connection.privmsg(channel, self.redis_read_last(parse_last[1]))
             else :
                 connection.privmsg(channel, "!last username")
         if msg.lower().startswith("!%s" % "dance"):
@@ -53,12 +64,13 @@ class TeddyBot (ircbot.SingleServerIRCBot):
             try:
                 browser = mechanize.Browser()
                 browser.open(msg)
-                self.memcache_write(source,msg)
+                self.redis_write(source,msg,browser.title())
+                self.redis_write_last(source,msg)
                 connection.privmsg(channel, browser.title())
             except IOError:
                 connection.privmsg(channel, "jason didnt design me with proper logic")
-            except:
-                connection.privmsg(channel, source + " what did you send me?")
+           # except:
+            #    connection.privmsg(channel, source + " what did you send me?")
          
 bot = TeddyBot ([( network, port )], nick, name)
 bot.start() 
